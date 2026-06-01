cb3b6d6d67c2d41e2aa0d6ab20f79d8bd44b76ea
karama2025 <Vertex.Logistics.LY@proton.me>
2026-06-01 23:01:44 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,20 @@
-const CACHE_NAME = 'karama-v1';
-const urlsToCache = [
-  '/karama/',
-  '/karama/index.html'
-];
+const CACHE = 'karama-v1';
+const FILES = ['./index.html'];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
